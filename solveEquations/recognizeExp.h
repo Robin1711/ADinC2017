b5155f5ae3c0a4c5b9beb72c3778b837d846d41a
Robin1711 <robin.sommer@planet.nl>
2017-02-16 13:49:44 +0100

    recognizeEq af en solveEquations bijna af

---
+++ solveEquations/recognizeExp.h
@@ -0,0 +1,22 @@
+/* recognizeExp.h, Gerard Renardel, 29 January 2014 */
+
+#ifndef RECOGNIZEEXP_H
+#define RECOGNIZEEXP_H
+
+
+int acceptNumber(List *lp);
+int acceptIdentifier(List *lp);
+int acceptCharacter(List *lp, char c);
+int acceptFactor(List *lp);
+int acceptTerm(List *lp);
+int acceptExpression(List *lp);
+int acceptEquation(List *lp);
+int isEqual(char *ar1, char*ar2, int size);
+int isOneVar(List *lp);
+int max(int a, int b);
+int degree(List *lp);
+void recognizeEquations();
+
+
+
+#endif
