e1426dc983ad1f9f493b275af1e8acd723944509
Robin1711 <robin.sommer@planet.nl>
2017-03-12 15:39:31 +0100

    start infix expressions

---
+++ infixExpressions/mainInf.c
@@ -0,0 +1,16 @@
+/* mainPref.c, Gerard Renardel, 10 December 2013
+ */
+
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* free */
+#include <assert.h> /* assert */
+#include "scanner.h"
+#include "recognizeExp.h"
+#include "infixExp.h"
+
+int main(int argc, char *argv[]) {
+  infExpTrees();
+  return 0;
+}
+
+
